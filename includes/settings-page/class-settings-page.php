f00ea27051da37ca273eb8d1801e56115d0ffb33
hisaveliy <dzvonkevich@gmail.com>
2019-07-17 11:55:13 +0300

    fix settings page errors

--- includes/settings-page/class-settings-page.php
+++ includes/settings-page/class-settings-page.php
@@ -6,275 +6,278 @@
  * Time: 22:41
  */
 
-namespace Plugin_Scope;
+namespace TourismTiger\TourCMS;
+
+use Hamcrest\Util;
 
 class Settings_Page {
 
 
-    public static $page_title = '';
+	public static $page_title = '';
+
+
+	public static $menu_title = '';
+
+
+	public static $page_slug = '';
+
+
+	protected static $instance = null;
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 * @throws \Exception
+	 */
+	public static function instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+
+	/**
+	 * @since 1.0.0
+	 * @throws \Exception
+	 */
+	public function __construct() {
+		self::$page_title = 'TourCMS Settings';
+		self::$menu_title = 'TourCMS';
+		self::$page_slug = PREFIX . '-settings';
+
+		add_action('admin_menu', __CLASS__ . '::add_admin');
+	}
+
+	public static function add_admin() {
+		$options = self::get_options();
+
+		if (array_key_exists('page', $_GET)
+		    && $_GET['page'] === self::$page_slug
+		    && array_key_exists('formaction', $_REQUEST)
+		    && 'save' === $_REQUEST['formaction']) {
+
+			foreach ($options as $value) {
+				if( isset( $_REQUEST[ $value['id'] ] ) ) {
+					update_option( $value['id'], $_REQUEST[ $value['id'] ]  );
+				} else {
+					delete_option( $value['id'] );
+				}
+			}
+
+			header("Location: themes.php?page=" . self::$page_slug . "&saved=true");
+			die;
+		}
+
+		add_options_page(
+			self::$page_title,
+			self::$menu_title,
+			'edit_posts',
+			self::$page_slug,
+			__CLASS__ . '::admin'
+		);
+	}
+
+
+	private static function get_id($name = '') {
+		return PREFIX . '_' . $name;
+	}
+
 
+	/**
+	 * @return array
+	 */
+	private static function get_options() {
+		return [
+			[
+				'label' => 'General',
+				'type' => 'headline-3',
+			],
+			[
+				'id'  => self::get_id('headline'),
+				'type'  => 'text',
+				'label' => 'Headline'
+			],
+			[
+				'id' => self::get_id('email'),
+				'type' => 'email',
+				'label' => 'Email'
+			],
+			[
+				'id' => self::get_id('password'),
+				'type' => 'password',
+				'label' => 'Password'
+			],
+		];
+	}
 
-    public static $menu_title = '';
 
+	/**
+	 * @return false|string
+	 */
+	private static function render_fields() {
+		$options = self::get_options();
 
-    public static $page_slug = '';
+		ob_start();
 
+		foreach ($options as $value) :
 
-    protected static $instance = null;
+			switch ($value['type']) :
+				case 'headline-3':
+					self::create_suf_headline_3($value);
+					break;
 
-    /**
-     * Return an instance of this class.
-     *
-     * @since     1.0.0
-     *
-     * @return    object    A single instance of this class.
-     * @throws \Exception
-     */
-    public static function instance() {
+				case 'headline-4':
+					self::create_suf_headline_4($value);
+					break;
 
-        // If the single instance hasn't been set, set it now.
-        if ( null == self::$instance ) {
-            self::$instance = new self;
-        }
+				case 'textarea':
+					self::create_section_for_textarea($value);
+					break;
 
-        return self::$instance;
-    }
+				case 'radio':
+					self::create_section_for_radio($value);
+					break;
 
+				default:
+					self::create_section_for_default($value);
+					break;
+			endswitch;
 
-    /**
-     * @since 1.0.0
-     * @throws \Exception
-     */
-    public function __construct() {
+		endforeach;
 
-        self::$page_title = 'Options';
-        self::$menu_title = 'Options';
-        self::$page_slug = PREFIX . '-settings';
+		return ob_get_clean();
+	}
 
-        add_action('admin_menu', __CLASS__ . '::add_admin');
+	private static function create_form() {
 
-    }
+		Utility::tpl('includes/settings-page/sections/form', [ 'fields' => self::render_fields() ]);
 
+	}
 
-    private static function get_id($name = '') {
-        return PREFIX . '_' . $name;
-    }
 
+	/**
+	 * @param array $value
+	 */
+	private static function create_opening_tag($value = array()) {
+		$group_class = "";
 
-    /**
-     * @return array
-     */
-    private static function get_options() {
-        return [
-            [
-                'label' => 'General',
-                'type' => 'headline-3',
-            ],
-            [
-                'id'  => self::get_id('headline'),
-                'type'  => 'text',
-                'label' => 'Headline'
-            ],
-            [
-                'id' => self::get_id('email'),
-                'type' => 'email',
-                'label' => 'Email'
-            ],
-            [
-                'id' => self::get_id('password'),
-                'type' => 'password',
-                'label' => 'Password'
-            ],
-        ];
-    }
+		if (isset($value['grouping'])) {
+			$group_class = "suf-grouping-rhs";
+		}
 
+		echo '<div class="suf-section fix">'."\n";
 
-    /**
-     * @return false|string
-     */
-    private static function render_fields() {
-        $options = self::get_options();
+		if ($group_class != "") {
+			echo "<div class='$group_class fix'>\n";
+		}
+	}
 
-        ob_start();
 
-        foreach ($options as $value) :
+	/**
+	 * @param array $value
+	 */
+	private static function create_closing_tag($value = array()) {
 
-            switch ($value['type']) :
-                case 'headline-3':
-                    self::create_suf_headline_3($value);
-                    break;
+		if (isset($value['desc']) && !(isset($value['type']) && $value['type'] == 'checkbox')) {
+			echo '<p>' . $value['desc']."</p><br />";
+		}
 
-                case 'headline-4':
-                    self::create_suf_headline_4($value);
-                    break;
+		if (isset($value['note'])) {
+			echo "<span class=\"note\">".$value['note']."</span><br />";
+		}
 
-                case 'textarea':
-                    self::create_section_for_textarea($value);
-                    break;
+		if (isset($value['grouping'])) {
+			echo "</div>\n";
+		}
 
-                case 'radio':
-                    self::create_section_for_radio($value);
-                    break;
+		echo "</div>\n";
+	}
 
-                default:
-                    self::create_section_for_default($value);
-                    break;
-            endswitch;
 
-        endforeach;
+	/**
+	 * @param array $value
+	 */
+	private static function create_suf_headline_3($value = array()) {
+		Utility::tpl('includes/settings-page/sections/headline-3', [ 'value' => $value ]);
+	}
 
-        return ob_get_clean();
-    }
 
-    private static function create_form() {
+	/**
+	 * @param array $value
+	 */
+	private static function create_suf_headline_4($value = array()) {
+		Utility::tpl('includes/settings-page/sections/headline-4', [ 'value' => $value ]);
+	}
 
-        Utility::tpl('includes/settings-page/sections/form', [ 'fields' => self::render_fields() ]);
+	/**
+	 * @param array $value
+	 */
+	private static function create_section_for_default($value = array()) {
+		self::create_opening_tag($value);
 
-    }
+		Utility::tpl('includes/settings-page/sections/default', [ 'value' => $value ]);
 
+		self::create_closing_tag($value);
+	}
 
-    /**
-     * @param array $value
-     */
-    private static function create_opening_tag($value = array()) {
-        $group_class = "";
 
-        if (isset($value['grouping'])) {
-            $group_class = "suf-grouping-rhs";
-        }
+	/**
+	 * @param array $value
+	 */
+	private static function create_section_for_textarea($value = array()) {
+		self::create_opening_tag($value);
 
-        echo '<div class="suf-section fix">'."\n";
+		Utility::tpl('includes/settings-page/sections/textarea', [ 'value' => $value ]);
 
-        if ($group_class != "") {
-            echo "<div class='$group_class fix'>\n";
-        }
-    }
+		self::create_closing_tag($value);
+	}
 
 
-    /**
-     * @param array $value
-     */
-    private static function create_closing_tag($value = array()) {
+	private static function create_section_for_radio($value = array()) {
+		self::create_opening_tag($value);
 
-        if (isset($value['desc']) && !(isset($value['type']) && $value['type'] == 'checkbox')) {
-            echo '<p>' . $value['desc']."</p><br />";
-        }
+		foreach ($value['options'] as $current_value => $label) {
+			Utility::tpl('includes/settings-page/sections/radio', [
+				'value' => $value,
+				'current_value' => $current_value,
+				'label' => $label,
+			]);
+		}
 
-        if (isset($value['note'])) {
-            echo "<span class=\"note\">".$value['note']."</span><br />";
-        }
+		self::create_closing_tag($value);
+	}
 
-        if (isset($value['grouping'])) {
-            echo "</div>\n";
-        }
+	public static function admin() {
 
-        echo "</div>\n";
-    }
+		if (array_key_exists('saved', $_REQUEST) && $_REQUEST['saved']) {
+			?>
 
+          <div id="message" class="updated fade">
+            <p><strong><?php _e( 'Settings updated', 'tourismtiger-tourcms-addon' ); ?></strong></p>
+          </div>
 
-    /**
-     * @param array $value
-     */
-    private static function create_suf_headline_3($value = array()) {
-        Utility::tpl('includes/settings-page/sections/headline-3', [ 'value' => $value ]);
-    }
+			<?php
+		}
+		?>
 
+      <div class="wrap">
+        <h1><?php _e( 'Savellab Simple Plugin Settings', 'tourismtiger-tourcms-addon' ); ?></h1>
 
-    /**
-     * @param array $value
-     */
-    private static function create_suf_headline_4($value = array()) {
-        Utility::tpl('includes/settings-page/sections/headline-4', [ 'value' => $value ]);
-    }
-
-    /**
-     * @param array $value
-     */
-    private static function create_section_for_default($value = array()) {
-        self::create_opening_tag($value);
-
-        Utility::tpl('includes/settings-page/sections/default', [ 'value' => $value ]);
-
-        self::create_closing_tag($value);
-    }
-
-
-    /**
-     * @param array $value
-     */
-    private static function create_section_for_textarea($value = array()) {
-        self::create_opening_tag($value);
-
-        Utility::tpl('includes/settings-page/sections/textarea', [ 'value' => $value ]);
-
-        self::create_closing_tag($value);
-    }
-
-
-    private static function create_section_for_radio($value = array()) {
-        self::create_opening_tag($value);
-
-        foreach ($value['options'] as $current_value => $label) {
-            Utility::tpl('includes/settings-page/sections/radio', [
-                'value' => $value,
-                'current_value' => $current_value,
-                'label' => $label,
-            ]);
-        }
-
-        self::create_closing_tag($value);
-    }
-
-    public static function add_admin() {
-        $options = self::get_options();
-
-        if ( $_GET['page'] === self::$page_slug && 'save' === $_REQUEST['formaction'] ) {
-
-            foreach ($options as $value) {
-                if( isset( $_REQUEST[ $value['id'] ] ) ) {
-                    update_option( $value['id'], $_REQUEST[ $value['id'] ]  );
-                } else {
-                    delete_option( $value['id'] );
-                }
-            }
-
-            header("Location: themes.php?page=" . self::$page_slug . "&saved=true");
-            die;
-        }
-
-        add_theme_page(
-            self::$page_title,
-            self::$menu_title,
-            'edit_themes',
-            self::$page_slug,
-            __CLASS__ . '::admin'
-        );
-    }
-
-    public static function admin() {
-
-        if ($_REQUEST['saved']) {
-            ?>
-
-            <div id="message" class="updated fade">
-                <p><strong><?php _e( 'Settings updated', '{Text_Domain}' ); ?></strong></p>
-            </div>
-
-            <?php
-        }
-        ?>
-
-        <div class="wrap">
-            <h1><?php _e( 'Savellab Simple Plugin Settings', '{Text_Domain}' ); ?></h1>
-
-            <div class="options">
-                <?php self::create_form(); ?>
-            </div><!-- mnt-options -->
-        </div><!-- wrap -->
-    <?php } // end function mynewtheme_admin()
+        <div class="options">
+			<?php self::create_form(); ?>
+        </div><!-- mnt-options -->
+      </div><!-- wrap -->
+	<?php } // end function mynewtheme_admin()
 }
 
 try {
-    Settings_Page::instance();
+	Settings_Page::instance();
 } catch (\Exception $e) {
 }
--- includes/settings-page/sections/default.php
+++ includes/settings-page/sections/default.php
@@ -6,24 +6,32 @@
  * Time: 22:51
  */
 
-$value = array_merge([
-    'id' => $value['id'],
-    'type' => 'text',
-    'label' => '',
-    'placeholder' => '',
-    'value' => get_option($value['id']) === false ? $value['std'] : get_option($value['id']),
-], $value);
-?>
+if (isset($value['id'])) :
 
-<label for="<?php echo $value['id']; ?>">
-    <?php echo $value['label']; ?>
+	if (!isset($value['std']))
+		$value['std'] = '';
+
+	$value = array_merge([
+		'id' => $value['id'],
+		'type' => 'text',
+		'label' => '',
+		'placeholder' => '',
+		'value' => get_option($value['id']) === false ? $value['std'] : get_option($value['id']),
+	], $value);
+	?>
+
+  <label for="<?php echo $value['id']; ?>">
+	  <?php echo $value['label']; ?>
 
     <br>
 
     <input
-        type="<?php echo $value['type']; ?>"
-        id="<?php echo $value['id']; ?>"
-        name="<?php echo $value['id']; ?>"
-        placeholder="<?php echo $value['placeholder']; ?>"
-        value="<?php echo $value['value']; ?>" />
-</label>
+      type="<?php echo $value['type']; ?>"
+      id="<?php echo $value['id']; ?>"
+      name="<?php echo $value['id']; ?>"
+      placeholder="<?php echo $value['placeholder']; ?>"
+      value="<?php echo $value['value']; ?>" />
+  </label>
+
+<?php
+endif;
